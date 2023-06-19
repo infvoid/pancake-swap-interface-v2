caf334e70657825da4676018b757b94ed285521f
infvoid <infvoid.defi@yandex.ru>
2023-06-19 17:56:35 +0800

    up

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1,5 +1,6 @@
 // import tokens from './tokens'
-import { PoolConfig, /*PoolCategory*/ } from './types'
+// import { PoolConfig, PoolCategory } from './types'
+import { PoolConfig } from './types'
 
 const pools: PoolConfig[] = [
 ]
